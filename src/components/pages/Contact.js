5699997fff65ef916b17e580b6f542173ba56efb
Kurt LaVacque <lavacquek@icloud.com>
2020-02-23 14:16:07 -0600

    Set up Contact Form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import Form from "../Form/Form"
+import Form from "../Form/Form"
 
 function Contact(props) {
     return (
         <div>
             <h1>Contact Page</h1>
-            {/* <Form /> */}
+            <Form />
         </div>
     );
 }
