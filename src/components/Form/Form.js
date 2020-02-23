a9f2e518d06f49394ed0a62234292565e138814a
Kurt LaVacque <lavacquek@icloud.com>
2020-02-23 14:42:42 -0600

    Working on making the emails send

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -23,6 +23,7 @@ class Form extends Component {
     handleFormSubmit = event => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         event.preventDefault();
+        const templateId = 'template_ATj7KFDu';
 
         // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
         alert(`Hello ${this.state.name} ${this.state.email} ${this.state.message}`)
@@ -31,7 +32,24 @@ class Form extends Component {
             email: this.state.email,
             message: this.state.message
         });
+        this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
     };
+    // handleSubmit(event) {
+    //     const templateId = 'template_ATj7KFDu';
+
+    //     this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
+    // }
+
+    sendFeedback(templateId, variables) {
+        window.emailjs.send(
+            'gmail', templateId,
+            variables
+        ).then(res => {
+            console.log('Email successfully sent!')
+        })
+            // Handle errors here however you like, or use a React error boundary
+            .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
 
     render() {
         // Notice how each input has a `value`, `name`, and `onChange` prop
@@ -47,6 +65,7 @@ class Form extends Component {
                         onChange={this.handleInputChange}
                         type="text"
                         placeholder="Name"
+                        id="name_i"
                     />
                     <input
                         value={this.state.email}
@@ -54,14 +73,15 @@ class Form extends Component {
                         onChange={this.handleInputChange}
                         type="text"
                         placeholder="Email"
+                        id="email_i"
                     />
                     <textarea
                         value={this.state.message}
                         name="message"
                         onChange={this.handleInputChange}
                         type="text"
-                        placeholder="Message">
-
+                        placeholder="Message"
+                        id="message_t">
                     </textarea>
                     <button onClick={this.handleFormSubmit}>Submit</button>
                 </form>
@@ -72,6 +92,79 @@ class Form extends Component {
 
 export default Form;
 
+// render() {
+//     return (
+//         <form className="test-mailing">
+//             <h1>Let's see if it works</h1>
+//             <div>
+//                 <textarea
+//                     id="test-mailing"
+//                     name="test-mailing"
+//                     onChange={this.handleChange}
+//                     placeholder="Post some lorem ipsum here"
+//                     required
+//                     value={this.state.feedback}
+//                     style={{ width: '100%', height: '150px' }}
+//                 />
+//             </div>
+//             <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+//         </form>
+//     )
+// }
+
+
+
+// export default Form;
+
+
+// import React from 'react';
+
+// export default class extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { feedback: '', name: 'Name', email: 'email@example.com' };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     render() {
+//         return (
+//             <form className="test-mailing">
+//                 <h1>Let's see if it works</h1>
+//                 <div>
+//                     <textarea
+//                         id="test-mailing"
+//                         name="test-mailing"
+//                         onChange={this.handleChange}
+//                         placeholder="Post some lorem ipsum here"
+//                         required
+//                         value={this.state.feedback}
+//                         style={{ width: '100%', height: '150px' }}
+//                     />
+//                 </div>
+//                 <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+//             </form>
+//         )
+//     }
+
+//     handleSubmit(event) {
+//         const templateId = 'template_id';
+
+//         this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
+//     }
+
+//     sendFeedback(templateId, variables) {
+//         window.emailjs.send(
+//             'gmail', templateId,
+//             variables
+//         ).then(res => {
+//             console.log('Email successfully sent!')
+//         })
+//             // Handle errors here however you like, or use a React error boundary
+//             .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+//     }
+
+// }
 
 // import React, { Component, useState } from "react";
 // // import "./style.css";
@@ -88,7 +181,7 @@ export default Form;
 //         // Getting the value and name of the input which triggered the change
 //         const { name, value } = event.target;
 
-        
+
 
 //         // Updating the input's state
 //         this.setState({
@@ -146,4 +239,4 @@ export default Form;
 //     }
 // }
 
-// export default Form;
+// export default Form
