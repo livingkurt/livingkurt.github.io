4daf1743a4e0fdc432fa68f876f9683add7b0907
Kurt LaVacque <lavacquek@icloud.com>
2020-02-23 21:23:35 -0600

    Made it so the contact page will send an email

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,96 +1,131 @@
-import React, { Component, useState } from "react";
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+// import './ContactUs.css';
 import "./form.css";
 
-class Form extends Component {
-    // Setting the component's initial state
-    state = {
-        name: "",
-        email: "",
-        message: ""
-    };
+export default function Form() {
 
-    handleInputChange = event => {
-        // Getting the value and name of the input which triggered the change
-        const { name, value } = event.target;
+  function sendEmail(e) {
+    e.preventDefault();
 
+    emailjs.sendForm('default_service', 'portfolio_email_template', e.target, 'user_9nPtsTdb3HAOWUZJUb04Q')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
-        // Updating the input's state
-        this.setState({
-            [name]: value
-        });
-    };
-
-    handleFormSubmit = event => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        event.preventDefault();
-        const templateId = 'template_ATj7KFDu';
-
-        // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-        alert(`Hello ${this.state.name} ${this.state.email} ${this.state.message}`)
-        this.setState({
-            name: this.state.name,
-            email: this.state.email,
-            message: this.state.message
-        });
-        this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
-    };
-    // handleSubmit(event) {
-    //     const templateId = 'template_ATj7KFDu';
-
-    //     this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
-    // }
-
-    sendFeedback(templateId, variables) {
-        window.emailjs.send(
-            'gmail', templateId,
-            variables
-        ).then(res => {
-            console.log('Email successfully sent!')
-        })
-            // Handle errors here however you like, or use a React error boundary
-            .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
-    }
-
-    render() {
-        // Notice how each input has a `value`, `name`, and `onChange` prop
-        return (
-            <div>
-                {/* <p>
-                    Hello {this.state.name} {this.state.email} {this.state.message} 
-                </p> */}
-                <form className="form">
-                    <input
-                        value={this.state.name}
-                        name="name"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="Name"
-                        id="name_i"
-                    />
-                    <input
-                        value={this.state.email}
-                        name="email"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="Email"
-                        id="email_i"
-                    />
-                    <textarea
-                        value={this.state.message}
-                        name="message"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="Message"
-                        id="message_t">
-                    </textarea>
-                    <button onClick={this.handleFormSubmit}>Submit</button>
-                </form>
-            </div>
-        );
-    }
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" placeholder="Name"/>
+      <label>Email</label>
+      <input type="email" name="user_email"placeholder="Email" />
+      <label>Message</label>
+      <textarea name="message" placeholder="Enter Message Here"/>
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
 
-export default Form;
+
+
+// import React, { Component, useState } from "react";
+// import "./form.css";
+
+// class Form extends Component {
+//     // Setting the component's initial state
+//     state = {
+//         name: "",
+//         email: "",
+//         message: ""
+//     };
+
+//     handleInputChange = event => {
+//         // Getting the value and name of the input which triggered the change
+//         const { name, value } = event.target;
+
+
+//         // Updating the input's state
+//         this.setState({
+//             [name]: value
+//         });
+//     };
+
+//     handleFormSubmit = event => {
+//         // Preventing the default behavior of the form submit (which is to refresh the page)
+//         event.preventDefault();
+//         const templateId = 'template_ATj7KFDu';
+
+//         // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+//         alert(`Hello ${this.state.name} ${this.state.email} ${this.state.message}`)
+//         this.setState({
+//             name: this.state.name,
+//             email: this.state.email,
+//             message: this.state.message
+//         });
+//         this.sendFeedback(templateId, { message_html: this.state.message, from_name: this.state.name, reply_to: this.state.email })
+//     };
+//     // handleSubmit(event) {
+//     //     const templateId = 'template_ATj7KFDu';
+
+//     //     this.sendFeedback(templateId, { message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email })
+//     // }
+
+//     sendFeedback(templateId, variables) {
+//         window.emailjs.send(
+//             'icloud', templateId,
+//             variables
+//         ).then(res => {
+//             console.log('Email successfully sent!')
+//         })
+//             // Handle errors here however you like, or use a React error boundary
+//             .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+//     }
+
+//     render() {
+//         // Notice how each input has a `value`, `name`, and `onChange` prop
+//         return (
+//             <div>
+//                 {/* <p>
+//                     Hello {this.state.name} {this.state.email} {this.state.message} 
+//                 </p> */}
+//                 <form className="form">
+//                     <input
+//                         value={this.state.name}
+//                         name="name"
+//                         onChange={this.handleInputChange}
+//                         type="text"
+//                         placeholder="Name"
+//                         id="name_i"
+//                     />
+//                     <input
+//                         value={this.state.email}
+//                         name="email"
+//                         onChange={this.handleInputChange}
+//                         type="text"
+//                         placeholder="Email"
+//                         id="email_i"
+//                     />
+//                     <textarea
+//                         value={this.state.message}
+//                         name="message"
+//                         onChange={this.handleInputChange}
+//                         type="text"
+//                         placeholder="Message"
+//                         id="message_t">
+//                     </textarea>
+//                     <button onClick={this.handleFormSubmit}>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Form;
 
 // render() {
 //     return (
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import Form from "../../Form"
 import Form from "../Form/Form"
 
 function Contact(props) {
