a6787da7e85db4798fa94d78afd6c6665d4e5e77
Kurt LaVacque <lavacquek@icloud.com>
2022-01-13 08:38:56 -0600

    Trying to clean up project

--- controllers/template_controller.ts
+++
@@ -1,9 +0,0 @@
-import { Content } from '../models';
-
-export default {
-	findAll: async (req: any, res: any) => {},
-	findById: async (req: any, res: any) => {},
-	create: async (req: any, res: any) => {},
-	update: async (req: any, res: any) => {},
-	remove: async (req: any, res: any) => {}
-};
